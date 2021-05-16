5b59914da8a6178ee4e4639e637161516e6429b0
Sathish Kumar <sathishkumar.aero51@gmail.com>
2021-05-17 01:02:43 +0530

    show loading meesages

--- src/utils/data-service.js
+++ src/utils/data-service.js
@@ -1,12 +1,15 @@
+import { message } from "antd";
 import { APP_ENV } from "./environment";
 
 export const DataService = {
   getHTMLData: (url) => {
+    const loader = message.loading("Loading state results...");
     return fetch(APP_ENV.dataURL + "/", {
       headers: {
         "Target-URL": url,
       },
     }).then((r) => {
+      loader();
       return r.text();
     });
   },
@@ -19,13 +22,18 @@ export const DataService = {
    * candidates: Candidate[ ̰]}}
    */
   getStatesConstituenciesAndCandidates: () => {
+    const hider = message.loading("Loading ECI data...");
     return fetch(APP_ENV.dataURL + "/getCandidateData")
       .then((response) => response.json())
       .then((result) => {
+        hider();
         if (!result.success) {
-          console.error(result.message);
+          const errorer = message.error(result.message);
+          setTimeout(errorer, 4000);
           return { states: [], constituencies: [], candidates: [] };
         } else {
+          const successor = message.success("Data loaded");
+          setTimeout(successor, 3000);
           return result.data;
         }
       });
